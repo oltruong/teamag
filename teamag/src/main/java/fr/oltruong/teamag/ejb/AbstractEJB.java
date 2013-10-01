9ca9d93cc03b43b56e61a4831011913a8d0b4a62
Olivier Truong <oliv.truong@gmail.com>
2013-10-01 15:13:26 +0200

    cleaning

--- teamag/src/main/java/fr/oltruong/teamag/ejb/AbstractEJB.java
+++ teamag/src/main/java/fr/oltruong/teamag/ejb/AbstractEJB.java
@@ -3,12 +3,15 @@ package fr.oltruong.teamag.ejb;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
-import ch.qos.logback.classic.Logger;
+import org.slf4j.Logger;
 
 public abstract class AbstractEJB {
 
     @Inject
     protected EntityManager entityManager;
+
+    // @PersistenceContext(unitName = "ejbPU")
+    // protected EntityManager entityManager;
 
     @Inject
     protected Logger logger;
