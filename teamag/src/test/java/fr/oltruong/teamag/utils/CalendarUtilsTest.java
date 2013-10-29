257aae6f91df76bed5a011974cd7f6e56d3b3685
Olivier Truong <oliv.truong@gmail.com>
2013-10-29 16:08:30 +0100

    Add CalendarUT

--- teamag/src/test/java/fr/oltruong/teamag/utils/CalendarUtilsTest.java
+++ teamag/src/test/java/fr/oltruong/teamag/utils/CalendarUtilsTest.java
@@ -1,7 +1,45 @@
 package fr.oltruong.teamag.utils;
 
+import static org.fest.assertions.Assertions.assertThat;
 
-public class CalendarUtilsTest
-{
+import java.util.Calendar;
+
+import org.junit.Test;
+
+public class CalendarUtilsTest {
+
+    @Test
+    public void testGetFirstDayOfMonth() {
+
+        Calendar cal = Calendar.getInstance();
+        assertThat(CalendarUtils.getFirstDayOfMonth(cal).get(Calendar.DAY_OF_MONTH)).isEqualTo(1);
+    }
+
+    @Test
+    public void testGetFirstDayOfMonth_null() {
+        assertThat(CalendarUtils.getFirstDayOfMonth(null)).isNull();
+    }
+
+    @Test
+    public void testGetNextDay() {
+        Calendar cal = Calendar.getInstance();
+        assertThat(CalendarUtils.getNextDay(cal).get(Calendar.DAY_OF_YEAR)).isEqualTo((cal.get(Calendar.DAY_OF_YEAR) + 1));
+    }
+
+    @Test
+    public void testGetNextDay_null() {
+        assertThat(CalendarUtils.getNextDay(null)).isNull();
+    }
+
+    @Test
+    public void testGetPreviousMonth() {
+        Calendar cal = Calendar.getInstance();
+        assertThat(CalendarUtils.getPreviousMonth(cal).get(Calendar.MONTH)).isEqualTo((cal.get(Calendar.MONTH) - 1));
+    }
+
+    @Test
+    public void testGetPreviousMonth_null() {
+        assertThat(CalendarUtils.getPreviousMonth(null)).isNull();
+    }
 
 }
