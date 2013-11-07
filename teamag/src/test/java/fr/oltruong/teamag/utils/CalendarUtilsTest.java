7290409764903ebc5db08502589f23c22da50973
Olivier Truong <oliv.truong@gmail.com>
2013-11-07 18:02:28 +0100

    Add UT

--- teamag/src/test/java/fr/oltruong/teamag/utils/CalendarUtilsTest.java
+++ teamag/src/test/java/fr/oltruong/teamag/utils/CalendarUtilsTest.java
@@ -2,37 +2,49 @@ package fr.oltruong.teamag.utils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeConstants;
-import org.junit.Test;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
 import java.util.List;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeConstants;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+
 public class CalendarUtilsTest {
 
     @Test
-    public void testGetWorkingDays() {
+    public void testGetWorkingDaysJanuary() {
 
-        DateTime january= DateTime.now().withMonthOfYear(1);
-        List <DateTime> workingDays = CalendarUtils.getWorkingDays(january);
+        testWorkingDays(1, Lists.newArrayList(1));
+        testWorkingDays(2, Lists.newArrayList(0));
+        testWorkingDays(4, Lists.newArrayList(1));
+        testWorkingDays(5, Lists.newArrayList(1, 8, 9, 20));
+        testWorkingDays(8, Lists.newArrayList(15));
+        testWorkingDays(11, Lists.newArrayList(1, 11));
+        testWorkingDays(12, Lists.newArrayList(25));
+
+    }
+
+    private void testWorkingDays(int monthNumber, Iterable<Integer> values) {
+        DateTime month = DateTime.now().withMonthOfYear(monthNumber);
+        List<DateTime> workingDays = CalendarUtils.getWorkingDays(month);
         assertThat(workingDays).isNotNull();
-        for (DateTime day:workingDays){
+        for (DateTime day : workingDays) {
             assertThat(day.getDayOfWeek()).isNotEqualTo(DateTimeConstants.SUNDAY).isNotEqualTo(DateTimeConstants.SATURDAY);
-            assertThat(day.getDayOfMonth()).isNotEqualTo(1);
+            assertThat(Integer.valueOf(day.getDayOfMonth())).isNotIn(values);
+
         }
     }
 
-
-        @Test
+    @Test
     public void testConstructorIsPrivate() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-            Constructor<CalendarUtils> constructor = CalendarUtils.class.getDeclaredConstructor();
-            assertThat(Modifier.isPrivate(constructor.getModifiers())).isTrue();
-            constructor.setAccessible(true);
-            constructor.newInstance();
+        Constructor<CalendarUtils> constructor = CalendarUtils.class.getDeclaredConstructor();
+        assertThat(Modifier.isPrivate(constructor.getModifiers())).isTrue();
+        constructor.setAccessible(true);
+        constructor.newInstance();
     }
 
 }
