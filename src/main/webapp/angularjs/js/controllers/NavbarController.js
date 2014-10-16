9759774a2ccb3a76a8f44f84dc5340298f6fd579
Olivier Truong <oliv.truong@gmail.com>
2014-10-16 08:05:21 +0200

    Enhance http header management

--- src/main/webapp/angularjs/js/controllers/NavbarController.js
+++ src/main/webapp/angularjs/js/controllers/NavbarController.js
@@ -11,12 +11,16 @@ teamagApp.controller('NavbarController', ['$scope', '$location', '$routeParams',
         };
 
         $scope.$watch(function () {
+            $http.defaults.headers.common.Authorization = userInfo.password;
+            $http.defaults.headers.common.userid = userInfo.id;
             return  userInfo;
         }, function (userInfoChanged) {
             $scope.loggedIn = userInfoChanged.loggedIn;
             $scope.name = userInfoChanged.name;
             $scope.admin = userInfoChanged.admin;
             $scope.supervisor = userInfoChanged.supervisor;
+            $http.defaults.headers.common.Authorization = userInfoChanged.password;
+            $http.defaults.headers.common.userid = userInfoChanged.id;
         }, true);
 
 
@@ -25,6 +29,8 @@ teamagApp.controller('NavbarController', ['$scope', '$location', '$routeParams',
             userInfo.admin = false;
             userInfo.supervisor = false;
             userInfo.name = undefined;
+            userInfo.id = undefined;
+            userInfo.password = undefined;
             $http.defaults.headers.common.Authorization = undefined;
             $http.defaults.headers.common.userid = undefined;
             $location.path('home');
