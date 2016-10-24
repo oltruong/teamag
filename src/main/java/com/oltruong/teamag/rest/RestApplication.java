75e5c83a745f31d7ffcd477bad59d3f253029fe6
Olivier Truong <oliv.truong@gmail.com>
2016-10-24 14:47:50 +0200

    Removing web.xml file

---
+++ src/main/java/com/oltruong/teamag/rest/RestApplication.java
@@ -0,0 +1,8 @@
+package com.oltruong.teamag.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/resources")
+public class RestApplication extends Application {
+}
