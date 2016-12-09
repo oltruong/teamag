1aa769b02cf1b168b005cf6ed1ab1228f36da57c
Olivier Truong <oliv.truong@gmail.com>
2016-12-09 08:53:28 +0100

    test email

--- src/test/java/com/oltruong/teamag/rest/ParameterEndPointTest.java
+++ src/test/java/com/oltruong/teamag/rest/ParameterEndPointTest.java
@@ -5,16 +5,17 @@ import com.oltruong.teamag.model.builder.EntityFactory;
 import com.oltruong.teamag.service.ParameterService;
 import com.oltruong.teamag.utils.TestUtils;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.subethamail.wiser.Wiser;
+import org.subethamail.wiser.WiserMessage;
 
-import java.util.Collections;
 import java.util.List;
 
 import javax.ws.rs.core.Response;
 
-import static javax.ws.rs.core.Response.ok;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
@@ -28,8 +29,11 @@ public class ParameterEndPointTest extends AbstractEndPointTest {
 
     private ParameterEndPoint parameterEndPoint;
 
+    private Wiser wiser;
+
     @Before
     public void prepare() {
+        wiser = new Wiser();
         super.setup();
         parameterEndPoint = new ParameterEndPoint();
         TestUtils.setPrivateAttribute(parameterEndPoint, mockParameterService, "parameterService");
@@ -37,6 +41,11 @@ public class ParameterEndPointTest extends AbstractEndPointTest {
         TestUtils.setPrivateAttribute(parameterEndPoint, AbstractEndPoint.class, mockUriInfo, "uriInfo");
         assertThat(parameterEndPoint.getService()).isEqualTo(mockParameterService);
 
+    }
+
+    @After
+    public void cleanup() {
+        wiser.stop();
     }
 
 
@@ -78,8 +87,45 @@ public class ParameterEndPointTest extends AbstractEndPointTest {
 
         checkResponseOK(response);
 
-        parameterList.forEach(parameter-> verify(mockParameterService).merge(parameter));
+        parameterList.forEach(parameter -> verify(mockParameterService).merge(parameter));
     }
+
+
+    @Test
+    public void sendMail() throws Exception {
+
+        Wiser wiser = new Wiser();
+        wiser.setPort(2500);
+
+        TestUtils.setPrivateAttribute(parameterEndPoint, 2500, "smtpPort");
+        wiser.start();
+
+
+        when(mockParameterService.getSmtpHost()).thenReturn("localhost");
+        when(mockParameterService.getAdministratorEmail()).thenReturn("admin@localhost.co");
+        final Response response = parameterEndPoint.testEmail();
+
+        checkResponseOK(response);
+        assertThat(wiser.getMessages()).hasSize(1);
+        final WiserMessage wiserMessage = wiser.getMessages().get(0);
+        assertThat(wiserMessage.getEnvelopeSender()).isEqualTo("admin@localhost.co");
+        assertThat(wiserMessage.getEnvelopeReceiver()).isEqualTo("admin@localhost.co");
+
+
+    }
+
+    @Test
+    public void sendMailIncorrectPort() throws Exception {
+
+        when(mockParameterService.getSmtpHost()).thenReturn("localhost");
+        when(mockParameterService.getAdministratorEmail()).thenReturn("admin@localhost.co");
+        final Response response = parameterEndPoint.testEmail();
+
+        checkResponseInternalError(response);
+
+
+    }
+
 
 
 }
