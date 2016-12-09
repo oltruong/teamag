cf708405e3c165cf2728f8cd22d51c47fbf4c12d
Olivier Truong <oliv.truong@gmail.com>
2016-12-09 13:22:07 +0100

    Cleanup

--- src/test/java/com/oltruong/teamag/rest/WorkLoadEndPointTest.java
+++ src/test/java/com/oltruong/teamag/rest/WorkLoadEndPointTest.java
@@ -6,18 +6,21 @@ import com.oltruong.teamag.model.builder.EntityFactory;
 import com.oltruong.teamag.service.BusinessCaseService;
 import com.oltruong.teamag.service.WorkLoadService;
 import com.oltruong.teamag.utils.TestUtils;
+import com.oltruong.teamag.webbean.WorkLoadContainer;
 import com.oltruong.teamag.webbean.WorkLoadMemberContainer;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import javax.ws.rs.core.Response;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -44,8 +47,20 @@ public class WorkLoadEndPointTest extends AbstractEndPointTest {
     }
 
     @Test
-    public void testUpdateWorkLoadNull() {
+    public void updateWorkLoadNull() {
         checkResponseOK(workLoadEndPoint.updateWorkLoad(null));
+    }
+
+
+    @Test
+    public void updateWorkLoad() {
+        WorkLoadContainer workLoadContainer = new WorkLoadContainer();
+        List<WorkLoad> workLoadList = new ArrayList<>();
+        workLoadContainer.setWorkLoadList(workLoadList);
+        checkResponseOK(workLoadEndPoint.updateWorkLoad(Collections.singletonList(workLoadContainer)));
+
+        verify(mockWorkLoadService).mergeList(eq(workLoadList));
+
     }
 
 
